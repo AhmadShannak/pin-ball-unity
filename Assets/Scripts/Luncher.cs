c628395edf21cfd880c565780ea50890950edf29
Ahmad Shannak <shannak.ahmad96@gmail.com>
2024-09-08 15:14:53 +0300

    connect to server

--- Assets/Scripts/Luncher.cs
+++ Assets/Scripts/Luncher.cs
@@ -2,16 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Pinball {
-  public class Luncher : MonoBehaviour {
+  public class Luncher : MonoBehaviourPunCallbacks {
     [SerializeField]
     Button playNowButton;
 
     private void Awake() {
       PhotonNetwork.AutomaticallySyncScene = true;
+      playNowButton.onClick.AddListener(Connect);
     }
 
     public void Connect() {
@@ -21,5 +23,20 @@ namespace Pinball {
         PhotonNetwork.ConnectUsingSettings();
       }
     }
+    
+#region MonoBehaviourPunCallbacks Callbacks
+    public override void OnConnectedToMaster() {
+      Debug.Log("OnConnectedToMaster() was called by PUN");
+    }
+    
+    public override void OnDisconnected(DisconnectCause cause) {
+      Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message) {
+      Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+      PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+#endregion
   }
 }
